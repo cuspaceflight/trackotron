414f84b3b18b68ef9df807ff7ab80562f025504a
Liyou Zhou <leothemagnificent@gmail.com>
2013-04-16 10:40:28 +0100

    add display of angle

--- interface/tracktronUI.py
+++ interface/tracktronUI.py
@@ -4,6 +4,7 @@ import sys
 import serialcommu
 from math import cos, sin, radians
 import threading
+import random
 
 class App:
     def __init__(self, master,mega1):
@@ -36,7 +37,6 @@ class App:
         self.ind_rect = self.w.create_rectangle(50, 25, 150, 75, fill="red")
 
     def update_online_variable(self):
-        #do some stuff
         def pinging(): self.online = self.mega1.ping()
         pinging_thread = threading.Thread( target = pinging )
         pinging_thread.start()
@@ -78,12 +78,10 @@ class dial(object):
         self.angle1 = self.angle2 = self.angle3 = 0
 
         # draw texts at the bottom
-        self.dial.create_rectangle( size/2-23, size-39, size/2+35, size-1,fill = "white" )
-        self.dial.create_text( size/2-20, size-1, text="Sensor", fill = "red", anchor="sw")
-        self.dial.create_text( size/2-20, size-12, text="Dead rec", fill = "blue", anchor="sw")
-        self.dial.create_text( size/2-20, size-23, text="Target", fill = "green", anchor="sw")
-
-
+        self.dial.create_rectangle( size/2-45, size-39, size/2+45, size-1,fill = "white" )
+        self.text1 = self.dial.create_text( size/2-43, size-1,  text="Sensor", fill = "red", anchor="sw")
+        self.text2 = self.dial.create_text( size/2-43, size-12, text="Dead",  fill = "blue", anchor="sw")
+        self.text3 = self.dial.create_text( size/2-43, size-23, text="Target", fill = "green", anchor="sw")
 
         # label the dial
         self.dial.create_text( size/2, 20, text=name, fill = "Black", anchor="s")
@@ -94,28 +92,30 @@ class dial(object):
         length = (size-2*offs)/2
 
         # update the readings of the pointers
-        self.angle1 = self.angle1 + 5
-        self.angle2 = self.angle2 + 3
-        self.angle3 = self.angle3 + 1
+        self.angle1 = (self.angle1 + (random.random()-0.3)*5)%360
+        self.angle2 = (self.angle2 + (random.random()-0.3)*5)%360
+        self.angle3 = (self.angle3 + (random.random()-0.3)*5)%360
 
-        def update_pointer(angle, ptr, ptr_txt):
+        def update_pointer(angle, ptr, ptr_txt, legend):
             x = length* cos(radians(angle-90))
             y = length* sin(radians(angle-90))
             self.dial.coords(ptr, size/2, size/2, x+size/2, y+size/2)
 
-            if (angle)%360 in range(90,270): anchor = "n"
+            if round(angle) in range(90,270): anchor = "n"
             else: anchor = "s"
 
-            if (angle)%360 in range(0,180): anchor += "w"
+            if round(angle) in range(0,180): anchor += "w"
             else: anchor += "e"
 
-            self.dial.itemconfig(ptr_txt, text = str((angle)%360), anchor = anchor)
+            self.dial.itemconfig(ptr_txt, text = str(round((angle)%360)), anchor = anchor)
             self.dial.coords(ptr_txt, x+size/2, y+size/2)
+            update_text = self.dial.itemcget(legend, "text").split()[0].ljust(10)
+            self.dial.itemconfig( legend, text = update_text + "%0.1f" %angle )
 
         # update the pointers
-        update_pointer(self.angle1,self.ptr1, self.ptr1_txt)
-        update_pointer(self.angle2,self.ptr2, self.ptr2_txt)
-        update_pointer(self.angle3,self.ptr3, self.ptr3_txt)
+        update_pointer(self.angle1,self.ptr1, self.ptr1_txt, self.text1)
+        update_pointer(self.angle2,self.ptr2, self.ptr2_txt, self.text2)
+        update_pointer(self.angle3,self.ptr3, self.ptr3_txt, self.text3)
 
         self.master.after(100, self.update_dial)
 
